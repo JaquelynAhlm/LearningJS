bc0cb7dcb225bb81ad0f16b57ddc9be7dcaade9e
JaquelynAhlm <46539991+JaquelynAhlm@users.noreply.github.com>
2019-01-11 01:17:21 -0600

    Add files via upload

---
+++ example_page/codingchallenge1.js
@@ -0,0 +1,14 @@
+/*******************************************
+ * All the Stuff 
+ * wow*/
+
+var markMass = 45
+var johnMass = 80
+var markH = 1.5
+var johnH = 1.8
+
+var markBMI = markMass / (markH * markH)
+var johnBMI = johnMass / (johnH * johnH)
+
+
+console.log("Is Mark's BMI higher than John's? " + (markBMI > johnBMI));
